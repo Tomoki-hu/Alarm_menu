6d5e2150dda74e741fd4a6b51f4825b0253c5306
okazaki05 <okazaki.yuuya0412@gmail.com>
2021-11-25 10:15:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/alarm_menu/AlarmListActivity.java
@@ -0,0 +1,40 @@
+package com.example.alarm_menu;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_list);
+
+        Button button6 = findViewById(R.id.button6);
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.alarm_list_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/alarm_menu/MainMenuActivity.java
@@ -0,0 +1,27 @@
+package com.example.alarm_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplication(), AlarmListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
